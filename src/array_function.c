e6470dab2810529f182cb3092f3459168dff8eff
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-10-07 21:32:10 +0700

    fix5

--- src/array_function.c
+++ src/array_function.c
@@ -5,7 +5,7 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
         dest[i] = src1[i];
     }
     for(size_t i = size_src1; i < size_src1+size_src2; i++){
-        dest[i] = src2[i];
+        dest[i] = src2[i-size_src1];
     }
 }
 
