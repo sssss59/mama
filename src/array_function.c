6a73b584caa1a1b3d3a8629ff6be3ff292967207
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-10-07 21:23:51 +0700

    fix3

--- src/array_function.c
+++ src/array_function.c
@@ -1,11 +1,10 @@
 #include "../include/array_function.h"
 
 void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
-    dest[size_src1+size_src2];
-    for(int i = 0; i < size_src1; i++){
+    for(size_t i = 0; i < size_src1; i++){
         dest[i] = src1[i];
     }
-    for(int i = size_src1; i < size_src1+size_src2; i++){
+    for(size_t i = size_src1; i < size_src1+size_src2; i++){
         dest[i] = src2[i];
     }
 }
