ff5fed10cbf985503bcf58f5f1e63b5cda2d3194
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-10-07 21:57:12 +0700

    fix6

--- src/array_function.c
+++ src/array_function.c
@@ -11,30 +11,52 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
     int array[size_src1+size_src2];
-    size_t i = 0;
-    size_t j = 0;
-    size_t c = 0;
-    while(i < size_src1 && j < size_src2){
-        if(src1[i] <= src2[j]){
-            array[c] = src1[i];
-        }else{
-            array[c] = src2[j];
+    if (size_src1 > size_src2){
+        for(size_t i = 0; i < size_src2; i++){
+            if(src1[i] < src2[i]){
+                array[i] = src1[i];
+            }else if(src1[i] == src2[i]){
+                array[i] = src1[i];
+                array[i+1] = src2[i];
+                i++;
+            }else if(src1[i] > src2[i]){
+                array[i] = src2[i];
+            }
         }
-        i++;
-        j++;
-        c++;
+        size_t m = size_src1 - size_src2;
+        for(size_t i = 0; i < m; i++){
+            array[size_src2 + i] = src1[size_src2 + i];
+        }
+    }else if(size_src2 == size_src2){
+        for(size_t i = 0; i < size_src2; i++){
+            if(src1[i] < src2[i]){
+                array[i] = src1[i];
+            }else if(src1[i] == src2[i]){
+                array[i] = src1[i];
+                array[i+1] = src2[i];
+                i++;
+            }else if(src1[i] > src2[i]){
+                array[i] = src2[i];
+            }
+        }
+    }else if(size_src1 < size_src2){
+        for(size_t i = 0; i < size_src1; i++){
+            if(src2[i] < src1[i]){
+                array[i] = src2[i];
+            }else if(src1[i]==src2[i]){
+                array[i] = src1[i];
+                array[i+1] = src2[i];
+                i++;
+            }else if(src2[i]>src1[i]){
+                array[i] = src1[i];
+            }
+        }
+        size_t m = size_src2 - size_src1;
+        for(size_t i = 0; i < m; i++){
+            array[size_src1 + i] = src1[size_src1 + i];
+        }
     }
-    while(i < size_src1){
-        array[c] = src1[i];
-        c++;
-        i++;
-    }
-    while(j < size_src2){
-        array[c] = src2[j];
-        c++;
-        j++;
-    }
-    for(int i = 0; i < size_src1+size_src2; i++){
+    for(int i = 0; i < sizeof(array)/sizeof(int); i++){
         dest[i] = array[i];
     }
 }
