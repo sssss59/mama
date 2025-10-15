bde07059cef96f33a084b0fb1c459295c367845b
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-10-16 00:03:46 +0700

    fix1

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -1,14 +1,31 @@
 #include "../include/advanced_array_function.h"
 
 int max_subarray_sum(int* nums, int size) {
-    return 0;
+    int max_sum = -2147483648;
+    for(int l = 0; l < size; l++){
+        for(int r = l; r < size; r++){
+            int temp_sum = 0;
+            int temp = l;
+            while(temp <= r){
+                temp_sum+= nums[temp];
+                temp++;
+            }
+            if(temp_sum > max_sum){
+                max_sum = temp_sum;
+            }
+        }
+    }
+    printf("%d", max_sum);
 }
 
 int length_of_lis(int* nums, int numsSize) {
+    printf("hz");
     return 0;
 }
 
 int* merge(int* intervals, int intervalsSize, int* returnSize) {
+    printf("hz");
     return NULL;
+
 }
 
